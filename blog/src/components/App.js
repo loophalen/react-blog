10e3654d393ae5dc92987de70f64ca4c88ad304d
blandhalen <blandhalen@blands-imac.home>
2018-08-19 17:54:18 -0400

    can render posts

--- blog/src/components/App.js
+++ blog/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react' 
 import {database} from '../firebase'
+import _ from 'lodash'
 
 
 class App extends Component {
@@ -7,10 +8,29 @@ class App extends Component {
     super(props)
     this.state ={
       title: '', 
-      body: ''
+      body: '', 
+      posts: {}
     }
     this.onInputChange = this.onInputChange.bind(this)
     this.onHandleSubmit = this.onHandleSubmit.bind(this)
+  }
+  componentDidMount(){
+    database.on('value', snapshot => {
+      this.setState({
+        posts: snapshot.val()
+      })
+    })
+  }
+  //pull data from firebase
+  renderPosts(){
+    return _ .map(this.state.posts, (post, key)=>{
+      return (
+      <div key={key}>
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+      </div>
+      )
+    })
   }
 
   onInputChange(event){
@@ -26,6 +46,10 @@ class App extends Component {
       body: this.state.body
     }
     database.push(post)
+    this.setState({
+      title:'', 
+      body:''
+    })
   }
 
   render() {
@@ -33,19 +57,23 @@ class App extends Component {
       <div className="container">
         <form onSubmit={this.onHandleSubmit}>
           <div className="form-input">
-            <input type="text" name="title" placeholder="Title" onChange={this.onInputChange} ref="title" 
+            <input 
+            value={this.state.title}
+            type="text" name="title" placeholder="Title" onChange={this.onInputChange} ref="title" 
             className="form-control"
             />
           </div>
           <div className="form-group">
-            <input type="text" name="body" placeholder="body" onChange={this.onInputChange} ref="body" 
+            <input 
+            value={this.state.body}
+            type="text" name="body" placeholder="body" onChange={this.onInputChange} ref="body" 
             className ="form-control"
             />
           </div>
           <button className="btn-btn-primary">Post</button>
         </form>
-  
-     
+        <br />
+        {this.renderPosts()}   
       </div>
     );
   }
